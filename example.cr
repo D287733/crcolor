57ee4473e58110c8f9027ffb2d3c8f0c0e50cad3
D287733 <97526512+D287733@users.noreply.github.com>
2022-07-17 19:10:08 +0200

    Create example.cr

---
+++ example.cr
@@ -0,0 +1,23 @@
+require "./crcolor" 
+
+
+system("clear")
+puts "black".black 
+puts "red".red      
+puts "green".green        
+puts "brown".brown
+puts "blue".blue        
+puts "magenta".magenta       
+puts "cyan".cyan           
+puts "gray".gray
+
+puts "\n\n"
+
+puts "black".bg_black 
+puts "red".bg_red 
+puts "green".bg_green        
+puts "brown".bg_brown
+puts "blue".bg_blue        
+puts "magenta".bg_magenta       
+puts "cyan".bg_cyan           
+puts "gray".bg_gray
